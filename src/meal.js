166d172e75b87eeb1646aafa630e1f6a925ec4ed
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-23 14:25:07 -0600

    Create two new functions: a nameMenuItem func and a createMenuItem func

--- src/meal.js
+++ src/meal.js
@@ -1,11 +1,25 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
+var burritoItem = nameMenuItem("burrito");
+var sushiItem = nameMenuItem("Pizza");
+var tacoItem = nameMenuItem("Taco");
+console.log(burritoItem, sushiItem, tacoItem);
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+  name : name,
+  price : price,
+  type : type
+  }
+  return menuItem;
+}
+console.log(createMenuItem("French toast", 5.00, 'breakfast'));
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
