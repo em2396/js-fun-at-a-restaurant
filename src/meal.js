ba8b2433ffc4f6a84e600fa47434b3734e6d902f
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-23 14:58:54 -0600

    Create a function that adds ingredients to an array and a function that formats price

--- src/meal.js
+++ src/meal.js
@@ -1,10 +1,10 @@
 function nameMenuItem(name) {
   return `Delicious ${name}`
 }
-var burritoItem = nameMenuItem("burrito");
-var sushiItem = nameMenuItem("Pizza");
-var tacoItem = nameMenuItem("Taco");
-console.log(burritoItem, sushiItem, tacoItem);
+// var burritoItem = nameMenuItem("burrito");
+// var sushiItem = nameMenuItem("Pizza");
+// var tacoItem = nameMenuItem("Taco");
+// console.log(burritoItem, sushiItem, tacoItem);
 
 function createMenuItem(name, price, type) {
   var menuItem = {
@@ -14,14 +14,33 @@ function createMenuItem(name, price, type) {
   }
   return menuItem;
 }
-console.log(createMenuItem("French toast", 5.00, 'breakfast'));
+var frenchToast = createMenuItem("French toast", 6.99, "breakfast");
+console.log(frenchToast);
+
+function addIngredients(newIngr, ingredientsArr){
+  if(ingredientsArr.includes(newIngr.toLowerCase())){
+    return ingredientsArr
+  } else {
+  ingredientsArr.push(newIngr);
+  return ingredientsArr;
+  }
+}
+var macAndCheeseIngredients = ['cheese', 'noodles', 'salt', 'pepper', 'cream cheese', 'milk'];
+console.log(addIngredients('mustard', macAndCheeseIngredients));
+console.log(addIngredients('cheese', macAndCheeseIngredients));
+console.log(addIngredients('CHeese', macAndCheeseIngredients));
+
+function formatPrice(price){
+  return `$${price}`;
+}
+console.log(formatPrice(frenchToast.price));
 
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
