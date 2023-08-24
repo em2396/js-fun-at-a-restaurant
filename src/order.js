f385b732ff8cdbf7a853d2c8529ceabb942c9bbe
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-24 15:49:33 -0600

    Create listItems function

--- src/order.js
+++ src/order.js
@@ -1,18 +1,72 @@
-function takeOrder(orderNumber, listOfOrders){
-  if(listOfOrders.length < 3){
-    listOfOrders.push(orderNumber);
-    return listOfOrders;
+function takeOrder(orderNumber, arrOfOrders){
+  if(arrOfOrders.length < 3){
+    arrOfOrders.push(orderNumber);
+    return arrOfOrders;
   } else {
-    return listOfOrders;
+    return arrOfOrders;
   }
 }
+var order1 = {
+  orderNumber : 1,
+  item: 'burger',
+  price : '8.99', 
+  orderType : 'delivery',
+  status : 'accepted'
+};
+var order2 = {
+  orderNumber : 2,
+  item : 'bit sandwich',
+  price : '5.99',
+  orderType : 'delivery',
+  status : 'accepted'
+}
+
+var order3 = {
+  orderNumber : 3,
+  item : 'reuben',
+  price : '8.99',
+  orderType : 'delivery',
+  status : 'accepted'
+}
+
+var takeOutOrder = [];
+takeOrder(order1, takeOutOrder);
+takeOrder(order2, takeOutOrder);
+takeOrder(order3, takeOutOrder);
+//console.log(takeOutOrder);
+
+function refundOrder(orderNumber, arrOfOrders){
+  for(var i = 0; i < arrOfOrders.length; i++){
+    if(arrOfOrders[i].orderNumber === orderNumber){
+      arrOfOrders.splice(i, 1);
+      //delete arrOfOrders[i];
+      //arrOfOrders = arrOfOrders.flat(1); 
+      //why doesn't this work?
+      }
+    }
+      return arrOfOrders;
+  }
+
+// console.log(refundOrder(2, takeOutOrder));
+// console.log(takeOutOrder)
+
+function listItems(arrOfOrders){
+  var itemName = [];
+  for(var i = 0; i < arrOfOrders.length; i++){
+    itemName.push(arrOfOrders[i].item)
+    //console.log(itemName)
+    var itemNames = itemName.join(', ')
+  }
+  return itemNames;
+}
+console.log(listItems(takeOutOrder));
 
 
 
 
 module.exports = {
   takeOrder,
-  // refundOrder,
-  // listItems,
+  refundOrder,
+  listItems,
   // searchOrder
 }
