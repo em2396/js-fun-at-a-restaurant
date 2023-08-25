888eca96d4f874ced1d81bde22cde05436a78a8e
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-24 18:20:15 -0600

    Start searchOrder function

--- src/order.js
+++ src/order.js
@@ -53,13 +53,42 @@ function refundOrder(orderNumber, arrOfOrders){
 function listItems(arrOfOrders){
   var itemName = [];
   for(var i = 0; i < arrOfOrders.length; i++){
-    itemName.push(arrOfOrders[i].item)
+    itemName.push(arrOfOrders[i].item);
     //console.log(itemName)
-    var itemNames = itemName.join(', ')
+    var itemNames = itemName.join(', ');
   }
   return itemNames;
 }
 console.log(listItems(takeOutOrder));
+
+function searchOrder(arrOfOrders, foodItem){
+  for(var i = 0; i < arrOfOrders.length; i++){
+    var foundFoodItem = arrOfOrders[i].item.includes(foodItem)
+    //return foundFoodItem;
+    }
+    return foundFoodItem;
+}
+// console.log(searchOrder(takeOutOrder, 'Pizza'));
+// var order1 = {
+//   orderNumber: 1234,
+//   item: "burger",
+//   price: "8.99",
+//   orderType: "delivery",
+//   status: "accepted"
+// };
+
+// var order2 = {
+//   orderNumber: 2342,
+//   item: "blt sandwich",
+//   price: "5.99",
+//   orderType: "delivery",
+//   status: "accepted"
+// };
+
+// var deliveryOrders = [order1, order2];
+// console.log(searchOrder(deliveryOrders, 'burger'), 'burger is present');
+// console.log(searchOrder(deliveryOrders, 'sushi'), 'sushi isnt present');
+
 
 
 
@@ -68,5 +97,5 @@ module.exports = {
   takeOrder,
   refundOrder,
   listItems,
-  // searchOrder
+  searchOrder
 }
