04f5ebbccffe6c05bdcb443d5b1f59d921135e56
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-25 13:08:39 -0600

    Create createRestaurant function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,12 +1,18 @@
 function createRestaurant(restName){
   var restObj = {
-    name : restName
+    name : restName,
+    menus : {
+      breakfast : [],
+      lunch : [],
+      dinner : []
+    }
   }
   return restObj
 }
 var sexyPizza = createRestaurant("sexy pizza");
 console.log(createRestaurant('Quiero Arepas'));
 console.log(sexyPizza);
+
 
 
 module.exports = {
